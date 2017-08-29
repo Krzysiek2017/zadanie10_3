403a687631d971390cfa3e000327cd517a25b74a
Logan <abba@gmail.com>
2017-08-30 01:00:31 +0200

    carousel

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+
+	var carouselList = $("#carousel ul");
+	var carInterval = setInterval(changeSlide, 3000);
+
+	function setTime() {
+		carInterval = setInterval(changeSlide, 3000)
+	}
+
+	function changeSlide() {
+		carouselList.animate({
+			'marginLeft': -400
+		}, 500, moveFirstSlide);
+	}
+
+	function moveFirstSlide() {
+		var firstItem = $('#carousel')
+			.find('li:first');
+		var lastItem = $('#carousel')
+			.find('li:last');
+
+		lastItem.after(firstItem);
+		carouselList.css({
+			marginLeft: 0
+		});
+	}
+});
